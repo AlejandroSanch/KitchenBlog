13589fa1edf1f31bf76cde2fe4112e31485a71a7
Alejandro <alejandrosanchrom@gmail.com>
2023-06-03 17:04:26 -0600

    initialize nhost backend

--- App.js
+++ App.js
@@ -4,11 +4,21 @@ import {View, Text} from "react-native";
 import DrawerNav from "./components/DrawerNav";
 import { NavigationContainer } from "@react-navigation/native";
 
+import { NhostClient, NhostProvider } from "@nhost/react";
+import * as SecureStore from "expo-secure-store";
+
+const nhost = new NhostClient({
+  backendUrl: "https://eqkexbkhlqgjrwoqlcmh.nhost.run",
+  clientStorageType: "expo-secure-storage",
+  clientStorage: SecureStore,
+});
+
 function App(){
   return(
+    <NhostProvider nhost={nhost}>
+      <DrawerNav/>
+    </NhostProvider>
 
-  <DrawerNav/>
-  
   )
 }
 
--- components/List.js
+++ components/List.js
@@ -3,18 +3,19 @@ import {View, ScrollView, StyleSheet} from "react-native";
 
 import Pin from "../components/Pin";
 
+const numRows = 2;
+
 const List = ({pins}) => {
     return(
         <ScrollView>
         {/* 1st column */}
             <View style={styles.container}>
-               <View style={styles.column}>
-                    {pins.filter((_,index)=> index % 2 === 0).map(pin => <Pin pin={pin} key={pin.id}/>)}
-               </View>
-        {/*2nd column*/}
-               <View style={styles.column}>
-               {pins.filter((_,index)=> index % 2 === 1).map(pin => <Pin pin={pin} key={pin.id}/>)}
-               </View>
+                {Array.from(Array(numRows)).map((_, colIndex) =>(
+                    <View style={styles.column} key={`column_${colIndex}`}>
+                        {pins.filter((_,index)=> index % numRows === colIndex).map(pin => 
+                        <Pin pin={pin} key={pin.id}/>)}
+                </View>
+                ))}
             </View>
         </ScrollView>
     );
